416d34faca22234a87aa0ef6b86ea7728a6624e5
Jimmy-02 <tainat1202@gmail.com>
2026-09-01 22:02:33 +0700

    feat: add protection

--- config/env.ts
+++ config/env.ts
@@ -15,3 +15,5 @@ export const DB_URI = getEnvVar("DB_URI");
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const JWT_SECRET = getEnvVar("JWT_SECRET");
 export const JWT_EXPIRES_IN = getEnvVar("JWT_EXPIRES_IN");
+export const ARCJET_KEY = getEnvVar("ARCJET_KEY");
+export const ARCJET_ENV = getEnvVar("ARCJET_ENV");
